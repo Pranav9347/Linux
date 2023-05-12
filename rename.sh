903f2b7ed859d4ccb7b4bb6cd111c4ef4dbe902c
Pranav <nadgirpranav624@gmail.com>
2023-05-12 18:19:47 +0530

    updated rename.sh to rename all .txt without asking for files to rename

--- rename.sh
+++ rename.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
-x='t'
-a=0
-b=1
-read -p "Enter file names with space(0 at the end): " -a x
-#taking input as an array '-a' option
-#x is the array variable varying from 1st file name to 0
-until [ ${x[$a]} = '0' ]
+a=1
+for filename in `ls *.txt`
 do
-  mv ${x[$a]} file${b}.txt
-  ((a++))
-((b++))
+   mv $filename file$a.txt
+a=$(($a+1))
 done
-
