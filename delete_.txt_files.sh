b12dffa2cb78478b4f4ea1afa682a3e60241d46c
Pranav <nadgirpranav624@gmail.com>
2023-05-12 18:18:55 +0530

    updated delete_txt_files_script to delete all .txt files in the 1st level sub-directories of the parent directory

--- delete_.txt_files.sh
+++ delete_.txt_files.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
+#nested loop to delete 
 # ls *.txt lists out all files ending with '.txt'. Then filename takes up each value
 #from the first to last and gets deleted
-for filename in `ls *.txt` #get the list of files
-do
-        rm $filename
-done
 
+#Deleting .txt files in the parent directory:
+for filename in `ls *.txt` #get the list of .txt files
+  do
+        rm $filename
+  done
+
+#Deleting .txt files in the 1st level sub-directories
+for folder in `ls -d */`
+do
+   cd $folder
+  for filename in `ls *.txt`
+  do
+        rm $filename
+  done
+cd ..
+done
